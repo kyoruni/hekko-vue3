eb5a8b7921797ea06001207a2285a954581ce744
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-13 00:49:57 +0900

    :wrench: イベントオブジェクトのテスト

--- hello-vue3/src/App.vue
+++ hello-vue3/src/App.vue
@@ -8,12 +8,38 @@ const changeTime = () => {
   nowRef.value = new Date().toLocaleTimeString();
 };
 
-// 1秒ごとに実行
-setInterval(changeTime, 1000);
+const bgColor = ref('orange');
+const message = ref('クリックしてね');
+
+const mousePointerX = ref(0);
+const mousePointerY = ref(0);
+
+// イベントオブジェクト以外を渡さない場合、呼び出し側では引数を書かなくて良い
+const onMouseMove = (event: MouseEvent) => {
+  mousePointerX.value = event.offsetX;
+  mousePointerY.value = event.offsetY;
+};
+
+// イベントオブジェクト以外も渡す場合、$eventの指定が必要
+const onClick = (event: MouseEvent, color: string) => {
+  message.value = event.timeStamp.toString();
+  bgColor.value = color;
+};
+
 </script>
 
 <template>
-  <div>
-    <p>今の時刻: {{ nowRef }}</p>
+  <div style="display: flex;">
+    <div class="section1">
+      <div style="width: 200px; height: 200px; background: red;" @mousemove="onMouseMove">
+        カーソルを乗せてね
+      </div>
+      <p>x：{{ mousePointerX }} / Y：{{ mousePointerY }}</p>
+    </div>
+    <div class="section2">
+      <div style="width: 200px; height: 200px;" :style="{background: bgColor}" @click="onClick($event, 'pink')">
+        {{ message }}
+      </div>
+    </div>
   </div>
 </template>
